b691abe80cfd6e581285f647e324f1dae9983324
Salim Benfarhat <hello@salim.link>
2023-11-19 11:11:09 +0100

    19/11/2023 - Adding Logic Structure for FrontController including web.json Routes

---
+++ app/core/Config.php
@@ -0,0 +1,5 @@
+<?php
+namespace PHP_SAB;
+class Config {
+    const BASE_URL = 'https://php-sab.lab';
+}
---
+++ app/init.php
@@ -0,0 +1,4 @@
+<?php
+require_once 'core/App.php';
+require_once 'core/Route.php';
+require_once 'controllers/FrontController.php';
