a60bc23be0ccf7f5fa93a48b0b42fe2829a9f5f7
Salim Benfarhat <hello@salim.link>
2023-11-19 11:25:26 +0100

    19/11/2023 - Adding Database core class
    + removing config.php on root
    + adding routes path and db config constants on Config core class
    + adding condition to check if connected or not on home view

--- app/core/Config.php
+++ app/core/Config.php
@@ -2,4 +2,10 @@
 namespace PHP_SAB;
 class Config {
     const BASE_URL = 'https://php-sab.lab';
+    const ROUTES_PATH = '../routes/web.json';
+    // Ajoutez ici vos informations de base de données
+    const DB_HOST = 'localhost';
+    const DB_NAME = 'php-sab';
+    const DB_USER = 'root';
+    const DB_PASS = '';
 }
--- app/init.php
+++ app/init.php
@@ -1,4 +1,6 @@
 <?php
 require_once 'core/App.php';
 require_once 'core/Route.php';
+require_once 'core/Config.php';
+require_once 'core/Database.php';
 require_once 'controllers/FrontController.php';
